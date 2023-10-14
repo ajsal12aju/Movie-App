131efe335c89c2e34c6f73c3edcd9d7374ada62c
Mohammed Ajsal <ajsal12aju@gmail.com>
2023-10-14 23:54:31 +0530

    updatetd

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import LandingPage from './LandingPage';
 function App() {
   return (
     <div className="App">
+      
        <LandingPage/>
     </div>
   );
--- src/LandingPage.js
+++ src/LandingPage.js
@@ -2,8 +2,23 @@ import React from 'react'
 
 function LandingPage() {
   return (
-    <div>
-      welcome to Landing page
+    <div> 
+        <form action="" className='search'>
+           <input type="search"  placeholder='Enter Movie Title' />
+        </form>
+
+        <ul className='movies'>
+            <li>
+<div className="movie">
+    <figure>
+<img src="https://image.tmdb.org/t/p/w300_and_h450_bestv2/kdPMUMJzyYAc4roD52qavX0nLIC.jpg" />
+    <figcaption>
+    <h2 className="movie__title">Talk to Me</h2>
+    </figcaption>
+    </figure>
+</div>
+            </li>
+        </ul>
     </div>
   )
 }
